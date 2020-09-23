82257787d0ddd24d6f68fde7562ee2dbefadb571
Igor Lima <igorlima1740@gmail.com>
2020-09-23 00:15:26 -0300

    modificacoes na classe Usuario

--- TP1/include/Usuario.h
+++ TP1/include/Usuario.h
@@ -1,16 +1,34 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 
 class Usuario
 {
     public:
         Usuario();
         virtual ~Usuario();
+        Usuario(string nome, string endereco, string cep, string cpf, string senha);
+        void SetSenha(string);
+        void VerInfoUsuario();
+
+        string GetNome();
+        string GetEndereco(); 
+        string GetCep(); 
+        string GetCpf(); 
+        string GetSenha(); 
 
     protected:
 
     private:
+        string nome;
+        string endereco;
+        string cep; 
+        string cpf; 
+        string senha;
 };
 
 #endif // USUARIO_H
--- TP1/src/Usuario.cpp
+++ TP1/src/Usuario.cpp
@@ -1,11 +1,52 @@
+#include <iostream>
 #include "Usuario.h"
+using namespace std;
 
-Usuario::Usuario()
+Usuario::Usuario(string nome, string endereco, string cep, string cpf, string senha)
 {
-    //ctor
+    nome = nome;
+    endereco = endereco;
+    cep = cep;
+    cpf = cpf;
+    SetSenha(senha);
 }
 
-Usuario::~Usuario()
+string Usuario::GetNome()
 {
-    //dtor
+    return nome;
 }
+
+string Usuario::GetEndereco()
+{
+    return endereco;
+}
+
+string Usuario::GetCep()
+{
+    return cep;
+}
+
+string Usuario::GetCpf()
+{
+    return cpf;
+}
+
+string Usuario::GetSenha()
+{
+    return senha;
+}
+
+void Usuario::SetSenha(string senha)
+{
+    senha = senha;
+}
+
+void Usuario::VerInfoUsuario()
+{
+    cout << "Informações do Usuário: \n";
+    cout << "Nome: " << this->GetNome();
+    cout << "Endereço: " << this->GetEndereco();
+    cout << "CEP: " << this->GetCep();
+    cout << "CPF: " << this->GetCpf();
+}
+
